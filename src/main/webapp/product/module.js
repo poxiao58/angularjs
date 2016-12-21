f6c64abf5b91bc317197b96c545c4f3d296c801e
poxiao58 <1782840861@qq.com>
2016-12-21 14:04:53 +0800

    修改module.js文件controller的写法

--- src/main/webapp/product/module.js
+++ src/main/webapp/product/module.js
@@ -1,6 +1,9 @@
 //module.jsp文件中所需的js文件
+/**
+ * 此操作是展示angularjs的指令module以及controller
+ */
 var app = angular.module('app', []);
-app.controller('myCtrl', function($scope) {
+app.controller('myCtrl', ['$scope',function($scope) {
     $scope.firstName= "LeBron";
     $scope.lastName= "James";
-});
+}]);
